706deb4c3a411868cfc9ecbf0b7648590662f9e8
jandrzejewski1 <janandrzejewski@aol.com>
2023-01-16 19:08:36 +0100

    testowa wersja z 3 flatami na input

--- m_flat_generator.py
+++ m_flat_generator.py
@@ -1,23 +1,32 @@
-import os
+from astropy import units as u
+from pathlib import Path
+from astropy.nddata import CCDData
+import numpy as np
+from astropy.stats import mad_std
+import ccdproc as ccdp
 from astropy.io import fits
-import cssproc
 
-def create_master_flat(folder):
-    flat_files = [f for f in os.listdir(folder) if f.endswith('.fits') and 'flat' in f]
 
-    # Tworzenie słownika z filtrami i odpowiadającymi im listami plików
-    filters = {}
-    for flat_file in flat_files:
-        hdu = fits.open(os.path.join(folder, flat_file))
-        filter_name = hdu[0].header['FILTER']
-        if filter_name in filters:
-            filters[filter_name].append(flat_file)
-        else:
-            filters[filter_name] = [flat_file]
-        hdu.close()
+def inv_median(a):
+    return 1 / np.median(a)
 
-    for filter_name, flat_list in filters.items():
-        master_flat = cssproc.median_combine(flat_list)
-        master_flat.writeto(os.path.join(folder, 'master_flat_' + filter_name + '.fits'))
 
-create_master_flat('/path/to/folder')
+    
+flat1_p = 'flat1.fits'
+flat2_p = 'flat2.fits'
+flat3_p = 'flat3.fits'
+flat1= CCDData(fits.getdata(flat1_p), meta=fits.getheader(flat1_p),unit = 'adu')
+flat2= CCDData(fits.getdata(flat2_p), meta=fits.getheader(flat2_p),unit = 'adu')
+flat3= CCDData(fits.getdata(flat3_p), meta=fits.getheader(flat3_p),unit = 'adu')
+
+
+to_combine = [flat1, flat2, flat3]
+
+
+combined_flat = ccdp.combine(to_combine,
+                                 method='median',
+                                 unit=u.adu, scale=inv_median,
+                                 sigma_clip=True, sigma_clip_low_thresh=5, sigma_clip_high_thresh=5,
+                                 sigma_clip_func=np.ma.median, signma_clip_dev_func=mad_std,
+                                )
+combined_flat.write('master_flat.fits', overwrite=True)
