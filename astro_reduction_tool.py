d913d813db4b4a43dd652e50eccca4e7c9dc6b4a
janandrzejewski <janandrzejewski@aol.com>
2023-02-02 19:58:23 +0100

    created a dictionary for easier management of m_flat

--- astro_reduction_tool.py
+++ astro_reduction_tool.py
@@ -15,57 +15,37 @@ logger.setLevel(logging.INFO)
 console_handler = logging.StreamHandler()
 console_handler.setLevel(logging.INFO)
 logger.addHandler(console_handler)
+logging.basicConfig(format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
 
-
-def read_path_into_CCDData(data_p, keys, filt=""):
+def read_path_into_CCDData(data_path, keys, _filter=None):
     ccd_list = []
-    if os.path.isdir(data_p):
-        ccd_p = ccdp.ImageFileCollection(data_p, keywords=keys)
-        if len(ccd_p.files_filtered(filter=filt)) != 0:
-            logger.info(
-                "Loading data from: %s len = %d",
-                data_p,
-                len(ccd_p.files_filtered(filter=filt)),
+
+    if not os.path.isdir(data_path): # better to add pattern  glob_include and ext parameters
+        logger.error(f"No directory {data_path}")
+        raise ValueError(f"No directory {data_path}")
+    ccd_collection = ccdp.ImageFileCollection(data_path, keywords=keys)
+    if _filter:
+        ccd_collection = ccd_collection.files_filtered(filter=_filter)
+    if len(ccd_collection) != 0:
+        logger.info(f"Loading data from: {data_path}")
+        for ccd_path in ccd_collection:
+            dir = os.path.join(data_path, ccd_path)
+            
+            ccd_file = CCDData(
+                fits.getdata(dir), meta=fits.getheader(dir, keywords=keys), unit="adu"
+                )
+
+            ccd_list.append(ccd_file)   
+            now = datetime.now()
+            formatted_time = now.strftime("[%Y-%m-%dT%H:%M:%S.%f]")
+            logging.info(
+                "%s  Loading %s file %s (1 of 1)",
+                formatted_time,
+                ccd_file.header["IMAGETYP"],
+                ccd_path,
             )
-            if filt != "":
-                ccd_p = ccd_p.files_filtered(filter=filt)
-            for h in range(0, len(ccd_p)):
-                dir = os.path.join(data_p, ccd_p[h])
-                now = datetime.now()
-                formatted_time = now.strftime("[%Y-%m-%dT%H:%M:%S.%f]")
-                ccd_file = CCDData(
-                    fits.getdata(dir),
-                    meta=fits.getheader(dir, keywords=keys),
-                    unit="adu",
-                )
-                ccd_list.append(ccd_file)
-                logger.info(
-                    "%s  Loading %s file %s (%d of %d)",
-                    formatted_time,
-                    ccd_file.header["IMAGETYP"],
-                    dir,
-                    h + 1,
-                    len(ccd_p),
-                )
-        return ccd_list
-    elif os.path.isfile(data_p):
-        now = datetime.now()
-        formatted_time = now.strftime("[%Y-%m-%dT%H:%M:%S.%f]")
-        ccd_file = CCDData(
-            fits.getdata(data_p), meta=fits.getheader(data_p, keywords=keys), unit="adu"
-        )
-        logging.info(
-            "%s  Loading %s file %s (1 of 1)",
-            formatted_time,
-            ccd_file.header["IMAGETYP"],
-            data_p,
-        )
-        return ccd_file
-    else:
-        logging.error(
-            "Wrong path. %s should be dir with .fits files or a path to a single .fits file",
-            data_p,
-        )
+
+    return ccd_list 
 
 
 def debias_dedark(file_list, master_dark, master_bias):
@@ -96,7 +76,7 @@ def inv_median(a):
 
 
 def master_flat_generator(flat_p, master_dark, master_bias, filt=""):
-    m_flat = {}
+    m_flat = []
     for f in filt:
         keys = ["imagetyp", "filter"]
         flat_list = read_path_into_CCDData(flat_p, keys, f)
@@ -117,27 +97,28 @@ def master_flat_generator(flat_p, master_dark, master_bias, filt=""):
         now = datetime.now()
         formatted_time = now.strftime("[%Y-%m-%dT%H:%M:%S.%f]")
         logging.info("%s  Created master_flat in %s filtr", formatted_time, f)
-        m_flat[f] = combined_flat
+        m_flat.append(combined_flat)
 
     return m_flat
 
 
 def folders_reduction(dir_folder, m_dark, m_bias, m_flat_list, filt, d):
     for f in filt:
+        
         ccd_folder = read_path_into_CCDData(
             os.path.join(dir_folder, d), ["imagetyp", "filter"], f
         )
         if len(ccd_folder) != 0:
-            # for ccd_file in ccd_folder:
-            # ccdp.subtract_bias(ccd_file, m_bias)
-            # # dark_subtracted = ccdp.subtract_dark(
-            # #     bias_subtracted,
-            # #     m_dark,
-            # #     exposure_time="EXPTIME",
-            # #     exposure_unit=u.adu,
-            # #     scale=True,
-            # # )
-            # ccdp.flat_correct(ccd_file, m_flat_list[f])
+            for ccd_file in ccd_folder:
+                bias_subtracted = ccdp.subtract_bias(ccd_file, m_bias)
+                dark_subtracted = ccdp.subtract_dark(
+                    bias_subtracted,
+                    m_dark,
+                    exposure_time="EXPTIME",
+                    exposure_unit=u.adu,
+                    scale=True,
+                )
+                ccd_file = ccdp.flat_correct(dark_subtracted, m_flat_list[f])
             now = datetime.now()
             formatted_time = now.strftime("[%Y-%m-%dT%H:%M:%S.%f]")
             logging.info("%s  Subtracted %s folder in %s filtr", formatted_time, d, f)
@@ -176,16 +157,23 @@ if __name__ == "__main__":
         for f in os.listdir(args.dir)
         if os.path.isdir(os.path.join(args.dir, f)) and f != "flat"
     ]
-    m_dark = read_path_into_CCDData(args.master_dark, ["imagetyp", "filter"])
-    m_bias = read_path_into_CCDData(args.master_bias, ["imagetyp", "filter"])
+    m_dark = CCDData(
+            fits.getdata(args.master_dark), meta=fits.getheader(args.master_dark), unit="adu"
+            )
+    m_bias = CCDData(
+            fits.getdata(args.master_bias), meta=fits.getheader(args.master_bias), unit="adu"
+            )
     flat_image_type = "flat"
     m_dark = ccdp.subtract_bias(m_dark, m_bias)
     flat_p = os.path.join(args.dir + "flat")
     ifc_flat = ccdp.ImageFileCollection(flat_p, keywords=["imagetyp", "filter"])
     flat_filters = set(h["filter"] for h in ifc_flat.headers(imagetyp=flat_image_type))
     m_flat_list = master_flat_generator(flat_p, m_dark, m_bias, flat_filters)
-    m_flat_list = []
-    for d in folders:
-        folders_reduction(
-            args.dir, args.master_dark, args.master_bias, m_flat_list, flat_filters, d
-        )
+    m_flat_dictionary= {"gs":[],"rs":[],"in":[],"Luminance":[],"B":[],"V":[],"R":[]};
+    for ccd_file in m_flat_list:
+        filter = ccd_file.header['filter']
+        m_flat_dictionary[filter]= ccd_file
+    # for d in folders:
+    #     folders_reduction(
+    #         args.dir, m_dark, m_bias, m_flat_list, flat_filters, d
+    #     )
